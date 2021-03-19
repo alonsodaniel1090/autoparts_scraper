aecb537c1f7237a71a7771f8ad5b347766dac9ac
alonsodaniel1090 <alonsodaniel1090@gmail.com>
2021-03-19 16:56:45 -0600

    Create three functions to make the main program simpler

---
+++ parts.py
@@ -0,0 +1,43 @@
+'''
+Helper functions to extract
+the information from a 
+website with BeautifulSoup
+'''
+
+def get_link(info):
+    '''
+    Gets a beautifulsoup object as info
+    and parse it to return the link of 
+    an specific product.
+    '''
+    href_start= info.find('href') + 6
+    href_finish = info.find('itemprop') - 2
+    href = info[href_start:href_finish] 
+    return href, href_start
+
+def get_title(info, reference):
+    '''
+    Gets a beautifulsoup object as info
+    and the start of a href tag as referene
+    to parse it and return the product title.
+    '''
+    title_start= info.find('title', reference) + 7
+    title_finish = info.find('>', title_start) - 1
+    title = info[title_start:title_finish]
+    return title
+ 
+def get_price(info):
+    '''
+    Gets a beautifulsoup objects as info 
+    and parse it to find the price of the 
+    product and then return it.
+    '''
+    price_start = info.find('<span') + 52
+    price_finish = info.find('</span>', price_start) - 2
+    price = str(info[price_start:price_finish])
+    return price.strip()
+
+
+
+
+
--- tools.py
+++ tools.py
@@ -1,44 +1,38 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
+import parts
 
 def main():
     '''Main program process'''
-    print('*' * 80)
 
     html = urlopen('https://refaccionariamario.com/categorias/1380-vw-sedan-tipo-1?id_category=1380&n=1264') 
     bs = BeautifulSoup(html.read(), 'html.parser')
     product_info = bs.find_all('div', {'class':'product-container'})
-     
-    # Get all the information for each product
-    info = str(product_info[0])
-
-    # href    
-    href_start= info.find('href') + 6
-    href_finish = info.find('itemprop') - 2
-    href = info[href_start:href_finish]
-    print(href)
-
-    print('\n')
-
-    # title
-    title_start= info.find('title', href_start) + 7
-    title_finish = info.find('>', title_start) - 1
-    title = info[title_start:title_finish]
-    print(title)
-
-    print('\n')
-
-    # price 
-    price_start = info.find('<span') + 52
-    price_finish = info.find('</span>', price_start) - 2
-    price = str(info[price_start:price_finish])
-    print(price.strip()) 
-
-
-
+    total_products = 1264 
 
     print('*' * 80)
+
+    for i in range(total_products - 1): 
+   
+        # Get all the information for each product
+        info = str(product_info[i])
+
+        # Get the href link and the index start of the href tag
+        href, href_start = parts.get_link(info)
+        print(href)
+        
+
+        # Get the product title
+        title = parts.get_title(info, href_start)
+        print(title)
+
+        
+        # Get the product title 
+        price = parts.get_price(info)
+        print(price)
+
+
+        print('*' * 80)
 
 
 
